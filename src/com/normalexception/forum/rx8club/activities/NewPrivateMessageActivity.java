8d9586ef760e1ece8dad8e1ae9f0b66cacb18429
Paimon Sorornejad <Paimon.Soror@gmail.com>
2012-11-21 15:31:56 -0500

    Added header info

--- src/com/normalexception/forum/rx8club/activities/NewPrivateMessageActivity.java
+++ src/com/normalexception/forum/rx8club/activities/NewPrivateMessageActivity.java
@@ -1,7 +1,30 @@
 package com.normalexception.forum.rx8club.activities;
 
+/************************************************************************
+ * NormalException.net Software, and other contributors
+ * http://www.normalexception.net
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ ************************************************************************/
+
 import org.jsoup.nodes.Document;
-import org.jsoup.select.Elements;
 
 import android.app.ProgressDialog;
 import android.os.Bundle;
@@ -64,6 +87,10 @@ public class NewPrivateMessageActivity extends ForumBaseActivity {
 		}
 	}
 	
+	/*
+	 * (non-Javadoc)
+	 * @see android.support.v4.app.FragmentActivity#onCreate(android.os.Bundle)
+	 */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -154,6 +181,10 @@ public class NewPrivateMessageActivity extends ForumBaseActivity {
    		return valid;
    	}
 
+   	/*
+   	 * (non-Javadoc)
+   	 * @see com.normalexception.forum.rx8club.activities.ForumBaseActivity#enforceVariants(int, int)
+   	 */
 	@Override
 	protected void enforceVariants(int currentPage, int lastPage) {	
 	}
