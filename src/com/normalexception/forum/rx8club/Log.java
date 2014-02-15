073d2887c7bd8c08b12be899db271ddfd7931c9d
Paimon Sorornejad <Paimon.Soror@gmail.com>
2014-02-14 22:03:23 -0500

    Reduce log size

--- src/com/normalexception/forum/rx8club/Log.java
+++ src/com/normalexception/forum/rx8club/Log.java
@@ -51,7 +51,7 @@ public class Log {
 	// Log4J Specific Methods
 	/////////////////////////////////////////////////////////
 	
-	private static final long MAX_SIZE = 1024 * 1024;
+	private static final long MAX_SIZE = 1024 * 10;
 	
 	/**
 	 * Configure log4j logging parameters
